88e92736b067fc7359763002f237d4c506ce6af8
Jose Alfonso Martinez Ramirez <jo.alfonso.martinez@gmail.com>
2024-04-12 22:49:13 -0600

    update

--- backtest.py
+++ backtest.py
@@ -10,6 +10,7 @@ import logging
 from functions import QAA
 
 
+# Class aun no funciona perfecto
 class BacktestingDinamico:
     def __init__(self, tickers, start_date, start_backtesting, end_date, frecuencias_rebalanceo_meses, rf, optimization_strategy, optimization_model, valor_portafolio_inicial):
         self.tickers = tickers
@@ -22,8 +23,8 @@ class BacktestingDinamico:
         self.optimization_model = optimization_model
         self.valor_portafolio_inicial = valor_portafolio_inicial
         self.resultados = []
-        self.datos = None
-        self.benchmark_datos = None
+        self.data = None
+        self.benchmark_data = None
 
     def run_backtesting(self):
         fecha_inicio = pd.to_datetime(self.start_backtesting)
@@ -45,13 +46,13 @@ class BacktestingDinamico:
         )
         estrategia.set_optimization_strategy(self.optimization_strategy)
         estrategia.set_optimization_model(self.optimization_model)
-        estrategia.cargar_datos()
-        estrategia.optimizar()
+        estrategia.load_data()
+        estrategia.optimize()
         
-        precios_actuales = estrategia.datos.iloc[-1]
-        pesos_optimos = estrategia.pesos_optimos
+        precios_actuales = estrategia.data.iloc[-1]
+        optimal_weights = estrategia.optimal_weights
         
-        valor_inversion_por_ticker = valor_portafolio * pesos_optimos
+        valor_inversion_por_ticker = valor_portafolio * optimal_weights
         num_acciones = (valor_inversion_por_ticker / precios_actuales).apply(np.floor)
         valor_invertido = num_acciones * precios_actuales
         cash_sobrante = valor_portafolio - valor_invertido.sum()
@@ -62,7 +63,7 @@ class BacktestingDinamico:
         fila_resultado = {
             'fecha_data_origen': self.start_date,
             'fecha_fin': fecha_rebalanceo_fin.strftime('%Y-%m-%d'),
-            **{f'peso_{ticker}': pesos_optimos[i] for i, ticker in enumerate(self.tickers)},
+            **{f'peso_{ticker}': optimal_weights[i] for i, ticker in enumerate(self.tickers)},
             **{f'acciones_{ticker}': num_acciones[ticker] for ticker in self.tickers},
             **{f'valor_{ticker}': valor_invertido[ticker] for ticker in self.tickers},
             'cash_sobrante': cash_sobrante,
@@ -87,17 +88,17 @@ class BacktestingDinamico:
                 )
                 estrategia.set_optimization_strategy(self.optimization_strategy)
                 estrategia.set_optimization_model(self.optimization_model)
-                estrategia.cargar_datos()
-                estrategia.optimizar()
+                estrategia.load_data()
+                estrategia.optimize()
                 
                 
-                precios_actuales = estrategia.datos.iloc[-1]
-                pesos_optimos = estrategia.pesos_optimos
+                precios_actuales = estrategia.data.iloc[-1]
+                optimal_weights = estrategia.optimal_weights
                 
                 if not num_acciones_anteriores.equals(pd.Series(0, index=self.tickers)):
                     valor_portafolio = (num_acciones_anteriores * precios_actuales).sum()
                 
-                valor_inversion_por_ticker = valor_portafolio * pesos_optimos
+                valor_inversion_por_ticker = valor_portafolio * optimal_weights
                 num_acciones = (valor_inversion_por_ticker / precios_actuales).apply(np.floor)
                 valor_invertido = num_acciones * precios_actuales
                 cash_sobrante = valor_portafolio - valor_invertido.sum()
@@ -110,7 +111,7 @@ class BacktestingDinamico:
                 fila_resultado = {
                     'fecha_data_origen': self.start_date,
                     'fecha_fin': fecha_rebalanceo_fin.strftime('%Y-%m-%d'),
-                    **{f'peso_{ticker}': pesos_optimos[i] for i, ticker in enumerate(self.tickers)},
+                    **{f'peso_{ticker}': optimal_weights[i] for i, ticker in enumerate(self.tickers)},
                     **{f'acciones_{ticker}': num_acciones[ticker] for ticker in self.tickers},
                     **{f'diff_{ticker}': diff_acciones[ticker] for ticker in self.tickers},
                     **{f'valor_{ticker}': valor_invertido[ticker] for ticker in self.tickers},
@@ -124,3 +125,105 @@ class BacktestingDinamico:
                     break
         
         return pd.DataFrame(self.resultados)
+    
+# DEF que funciona mejor
+
+def backtesting_dinamico(tickers, start_date, start_backtesting, end_date, frecuencias_rebalanceo_meses, rf, optimization_strategy, optimization_model, valor_portafolio_inicial):
+    fecha_inicio = pd.to_datetime(start_backtesting)
+    fecha_fin = pd.to_datetime(end_date)
+    valor_portafolio = valor_portafolio_inicial
+    num_acciones_anteriores = pd.Series(0, index=tickers)
+    resultados = []
+    
+    # Proceso inicial en start_backtesting
+    fecha_actual = fecha_inicio
+    fecha_rebalanceo_fin = fecha_actual   
+    if fecha_rebalanceo_fin > fecha_fin:
+        fecha_rebalanceo_fin = fecha_fin
+    
+    estrategia = QAA(
+        tickers=tickers,
+        start_date=start_date,  # Datos históricos desde start_date
+        end_date=fecha_rebalanceo_fin.strftime('%Y-%m-%d'),
+        rf=rf
+    )
+    estrategia.set_optimization_strategy(optimization_strategy)
+    estrategia.set_optimization_model(optimization_model)
+    estrategia.load_data()
+    estrategia.optimize()
+    
+    precios_actuales = estrategia.data.iloc[-1]
+    optimal_weights = estrategia.optimal_weights
+    
+    valor_inversion_por_ticker = valor_portafolio * optimal_weights
+    num_acciones = (valor_inversion_por_ticker / precios_actuales).apply(np.floor)
+    valor_invertido = num_acciones * precios_actuales
+    cash_sobrante = valor_portafolio - valor_invertido.sum()
+    
+    num_acciones_anteriores = num_acciones.copy()
+    valor_portafolio = valor_invertido.sum() + cash_sobrante
+    
+    fila_resultado = {
+        'fecha_data_origen': start_date,
+        'fecha_fin': fecha_rebalanceo_fin.strftime('%Y-%m-%d'),
+        **{f'peso_{ticker}': optimal_weights[i] for i, ticker in enumerate(tickers)},
+        **{f'acciones_{ticker}': num_acciones[ticker] for ticker in tickers},
+        **{f'valor_{ticker}': valor_invertido[ticker] for ticker in tickers},
+        'cash_sobrante': cash_sobrante,
+        'valor_total_cartera': valor_portafolio
+    }
+    
+    resultados.append(fila_resultado)
+    
+    # Continuación del proceso de rebalanceo después del inicio
+    for frecuencia_rebalanceo_meses in frecuencias_rebalanceo_meses:
+        fecha_actual = fecha_rebalanceo_fin
+        while fecha_actual <= fecha_fin:
+            fecha_rebalanceo_fin = fecha_actual + relativedelta(months=frecuencia_rebalanceo_meses)
+            if fecha_rebalanceo_fin > fecha_fin:
+                fecha_rebalanceo_fin = fecha_fin
+            
+            estrategia = QAA(
+                tickers=tickers, 
+                start_date=start_date, 
+                end_date=fecha_rebalanceo_fin.strftime('%Y-%m-%d'), 
+                rf=rf
+            )
+            estrategia.set_optimization_strategy(optimization_strategy)
+            estrategia.set_optimization_model(optimization_model)
+            estrategia.load_data()
+            estrategia.optimize()
+            
+            precios_actuales = estrategia.data.iloc[-1]
+            optimal_weights = estrategia.optimal_weights
+            
+            if not num_acciones_anteriores.equals(pd.Series(0, index=tickers)):
+                valor_portafolio = (num_acciones_anteriores * precios_actuales).sum()
+            
+            valor_inversion_por_ticker = valor_portafolio * optimal_weights
+            num_acciones = (valor_inversion_por_ticker / precios_actuales).apply(np.floor)
+            valor_invertido = num_acciones * precios_actuales
+            cash_sobrante = valor_portafolio - valor_invertido.sum()
+            
+            diff_acciones = num_acciones - num_acciones_anteriores
+            
+            num_acciones_anteriores = num_acciones.copy()
+            valor_portafolio = valor_invertido.sum() + cash_sobrante
+            
+            fila_resultado = {
+                'fecha_data_origen': start_date,
+                'fecha_fin': fecha_rebalanceo_fin.strftime('%Y-%m-%d'),
+                **{f'peso_{ticker}': optimal_weights[i] for i, ticker in enumerate(tickers)},
+                **{f'acciones_{ticker}': num_acciones[ticker] for ticker in tickers},
+                **{f'diff_{ticker}': diff_acciones[ticker] for ticker in tickers},
+                **{f'valor_{ticker}': valor_invertido[ticker] for ticker in tickers},
+                'cash_sobrante': cash_sobrante,
+                'valor_total_cartera': valor_portafolio
+            }
+            
+            resultados.append(fila_resultado)
+            fecha_actual = fecha_rebalanceo_fin
+            if fecha_rebalanceo_fin == fecha_fin:
+                break
+    
+    return pd.DataFrame(resultados)
