7f7b8be81e3933e677c2a8c6fcc18ad10f44d35c
Juan Antonio Mugica Liparoli <toonymlp@gmail.com>
2024-04-28 16:19:06 -0600

    Mejora formato streamlit y cambios en las paginas =)

--- streamlit_pages/basic.py
+++ streamlit_pages/basic.py
@@ -5,108 +5,107 @@ import plotly.express as px
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from backtest import dynamic_backtesting
+import random
 
-list_strategy = ['Minimum Variance', 'Omega Ratio', 'Semivariance', #'Martingale', 
+list_strategy = ['Minimum Variance', 'Omega Ratio', 'Semivariance',
                  'Roy Safety First Ratio', 'Sortino Ratio', 'Fama French', 'CVaR', 
                  'HRP', 'Sharpe Ratio', 'Black Litterman', 'Total Return']
 
-def show_basic():
-    st.title('Backtesting Page')
-    st.write("Welcome to the QAA Strategy Backtesting.")
+def validate_date(date_text):
+    try:
+        return datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        return None
 
+def parse_int_from_string(input_string):
+    try:
+        return int(input_string.replace(',', ''))
+    except ValueError:
+        return None
+
+@st.cache_data(show_spinner=False)
+def run_backtesting(tickers, start_date, start_backtesting, end_date, frequency, rf, initial_value, commission):
+    strategy_results = {}
+    for i, strategy in enumerate(list_strategy):
+        results, daily_data, portfolio_values = dynamic_backtesting(
+            tickers=tickers,
+            start_date_data=start_date,
+            start_backtesting=start_backtesting,
+            end_date=end_date,
+            rebalance_frequency_months=frequency,
+            rf=rf,
+            optimization_strategy=strategy,
+            optimization_model='SLSQP',
+            initial_portfolio_value=initial_value,
+            commission=commission
+        )
+        strategy_results[strategy] = (results, daily_data, portfolio_values)
+    return strategy_results
+
+def display_results(strategy_results, show_all_strategies):
+    sorted_strategies = sorted(strategy_results.items(), key=lambda x: x[1][2][-1], reverse=True)
+    if show_all_strategies:
+        strategies_to_display = sorted_strategies
+    else:
+        strategies_to_display = random.sample(sorted_strategies, 4)
+
+    for strategy, (result_df, daily_data, portfolio_values) in strategies_to_display:
+        st.markdown(f"### {strategy}")
+        st.dataframe(result_df.assign(total_portfolio_value=lambda x: x['total_portfolio_value'].apply("${:,.2f}".format)))
+        plot_strategy_performance(daily_data, portfolio_values, strategy)
+        final_value = portfolio_values[-1]
+        st.metric(label="Final Portfolio Value", value=f"${final_value:,.2f} (USD)")
+        st.markdown("---")
+
+def plot_strategy_performance(daily_data, portfolio_values, strategy_name):
+    colors = px.colors.qualitative.Plotly
+    strategy_index = list_strategy.index(strategy_name) % len(colors)
+    fig = px.line(
+        x=daily_data.index, 
+        y=portfolio_values, 
+        title=f"{strategy_name} - Valor del Portafolio a lo largo del Backtesting",
+        labels={"x": "Date", "y": "Valor del Portafolio ($)"},
+        line_shape='linear'
+    )
+    fig.update_traces(line=dict(color=colors[strategy_index]))
+    st.plotly_chart(fig, use_container_width=True)
+
+def show_basic():
+    st.title('Backtesting Sencillo (Un método)')
+    show_all_strategies = st.checkbox("Mostrar más estrategias")
     with st.form("input_form"):
         col1, col2, col3 = st.columns(3)
         with col1:
-            tickers = st.text_input("Enter tickers separated by commas", "ABBV, MET, OXY, PERI")
-            start_date_data = st.text_input("Start date for historical data", "2020-01-02")
-            start_backtesting = st.text_input("Start date for backtesting", "2023-01-23")
-            end_date = st.text_input("End date for backtesting", "2024-01-23")
+            tickers = st.text_input("Tickers:", "ABBV, MET, OXY, PERI")
+            start_date_data = st.text_input("Fecha histórica:", "2020-01-02")
+            start_backtesting = st.text_input("Fecha inicio:", "2023-01-23")
+            end_date = st.text_input("Fecha final:", "2024-01-23")
         with col2:
-            rebalance_frequency_months = st.slider("Rebalance frequency in months", 1, 12, 6)
-            rf = st.number_input("Enter the risk-free rate", value=0.02)
+            rebalance_frequency_months = st.slider("Frecuencia de rebalanceo (M):", 1, 12, 6)
+            rf = st.number_input("Tasa libre de riesgo:", value=0.02, format="%.4f")
         with col3:
-            initial_portfolio_value = st.number_input("Initial portfolio value", value=1000000)
-            commission = st.number_input("Transaction commission", value=0.0025)
+            initial_portfolio_value = st.text_input("Valor del portafolio:", value='1,000,000')
+            commission = st.number_input("Comisión por transacción:", value=0.0025, format="%.4f")
 
-        submitted = st.form_submit_button("Run Backtesting")
+        submitted = st.form_submit_button("Ejecutar Backtesting....")
 
-        if submitted:
-            strategy_results = {}
-            progress_text = st.empty()  # Placeholder for dynamic text
-            progress_bar = st.progress(0)
-            num_strategies = len(list_strategy)
+    if submitted:
+        if not (validate_date(start_date_data) and validate_date(start_backtesting) and validate_date(end_date)):
+            st.error("Ingresa valores válidos en el siguiente formato: YYYY-MM-DD")
+            return
 
-            for i, strategy in enumerate(list_strategy):
-                tickers_list = [ticker.strip() for ticker in tickers.split(',')]
-                resultados_backtesting, daily_data, portfolio_values = dynamic_backtesting(
-                    tickers=tickers_list,
-                    start_date_data=start_date_data,
-                    start_backtesting=start_backtesting,
-                    end_date=end_date,
-                    rebalance_frequency_months=rebalance_frequency_months, 
-                    rf=rf, 
-                    optimization_strategy=strategy, 
-                    optimization_model='SLSQP',  # Assuming a single model for simplicity
-                    initial_portfolio_value=initial_portfolio_value,
-                    commission=commission
-                )
-                strategy_results[strategy] = (resultados_backtesting, daily_data, portfolio_values)
-                current_progress = (i + 1) / num_strategies
-                progress_bar.progress(current_progress)
-                progress_text.text(f"Processing: {int(current_progress * 100)}% Complete")  # Update text
-            
-            progress_text.text("All strategies have been processed. Displaying results...")
-            sorted_results = sorted(strategy_results.items(), key=lambda item: item[1][0]["total_portfolio_value"].iloc[-1], reverse=True)
+        tickers_list = [ticker.strip() for ticker in tickers.split(',')]
+        if len(tickers_list) < 2:
+            st.error("Ingrese por lo menos 2 tickers válidos.")
+            return
 
-            # Plot all strategies' portfolio values
-            plot_all_strategies(dict(sorted_results))
+        parsed_initial_value = parse_int_from_string(initial_portfolio_value)
+        if parsed_initial_value is None:
+            st.error("Ingresa un número válido para el Valor inicial del Portafolio.")
+            return
 
-            # Display DataFrames and Highlight Total Portfolio Values
-            for strategy, (result_df, daily_data, portfolio_values) in sorted_results:
-                col1, col2 = st.columns([3, 1])
-                with col1:
-                    st.write(f"Results for {strategy}:")
-                    st.dataframe(result_df)
-                with col2:
-                    st.metric(label="Total Portfolio Value", value=f"${result_df['total_portfolio_value'].iloc[-1]:,.2f}")
+        strategy_results = run_backtesting(tickers_list, start_date_data, start_backtesting, end_date,
+                                           rebalance_frequency_months, rf, parsed_initial_value, commission)
+        display_results(strategy_results, show_all_strategies)
 
-def plot_all_strategies(strategy_results):
-    # Create a line plot for portfolio values
-    line_fig = px.line()
-
-    # We will first identify the best performing strategy based on the last 'total_portfolio_value'
-    best_strategy = None
-    highest_value = -float('inf')  # Initialize with a very low number
-
-    for strategy, (result_df, daily_data, portfolio_values) in strategy_results.items():
-        line_fig.add_scatter(x=daily_data.index, y=portfolio_values, mode='lines', name=strategy)
-        # Determine if this strategy has the highest ending portfolio value
-        if result_df['total_portfolio_value'].iloc[-1] > highest_value:
-            highest_value = result_df['total_portfolio_value'].iloc[-1]
-            best_strategy = (strategy, result_df)
-
-    line_fig.update_layout(title="Portfolio Value Over Time by Strategy",
-                           xaxis_title='Date',
-                           yaxis_title='Portfolio Value ($)',
-                           legend_title='Strategy')
-
-    # Now plot only the pie chart for the best strategy
-    if best_strategy:
-        strategy_name, result_df = best_strategy
-        last_weights = {col.split('_')[-1]: result_df[col].iloc[-1] for col in result_df.columns if col.startswith('weight_')}
-        pie_labels = list(last_weights.keys())
-        pie_values = list(last_weights.values())
-        
-        # Custom color palette for the pie chart
-        custom_colors = px.colors.qualitative.Pastel1
-        
-        pie_fig = px.pie(names=pie_labels, values=pie_values, title=f'Asset Weights for {strategy_name}',
-                         color_discrete_sequence=custom_colors)
-
-    # Use more space for the line chart than for the pie chart
-    col1, col2 = st.columns([3, 1])  # Adjusting column width proportions here
-    with col1:
-        st.plotly_chart(line_fig, use_container_width=True)
-    with col2:
-        if best_strategy:
-            st.plotly_chart(pie_fig, use_container_width=True)
+show_basic()
--- streamlit_pages/qaa.py
+++ streamlit_pages/qaa.py
@@ -1,7 +1,70 @@
 import streamlit as st
 from functions import QAA
-
+import pandas as pd
 
 def show_qaa():
-    st.title('Página de QAA')
-    st.write("Aquí puedes calcular diferentes estrategias de QAA.")
+    st.title('Análisis de Estrategias de Optimización de Cartera')
+    st.write("Selecciona los parámetros y calcula los pesos óptimos para diferentes estrategias y modelos de optimización.")
+
+    with st.form("input_form"):
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            tickers = st.text_input("Tickers:", "ABBV, MET, OXY, PERI")
+            start_date_data = st.text_input("Fecha de inicio:", "2020-01-02")
+            end_date_data = st.text_input("Fecha de término:", "2024-01-23")
+
+        with col2:
+            rf = st.number_input("Tasa libre de riesgo:", value=0.02)
+
+        with col3:
+            lower_bound = st.number_input("Límite inferior:", value=0.10)
+            higher_bound = st.number_input("Límite superior:", value=0.99)
+
+        submitted = st.form_submit_button("Calcular Todas las Estrategias")
+
+    if submitted:
+        calculate_all_strategies(tickers, start_date_data, end_date_data, rf, lower_bound, higher_bound)
+
+def calculate_portfolio_metrics(qaa_instance):
+    portfolio_return = qaa_instance.calculate_portfolio_return()
+    portfolio_volatility = qaa_instance.calculate_portfolio_volatility()
+    return portfolio_return, portfolio_volatility
+
+
+
+def calculate_all_strategies(tickers, start_date, end_date, rf, lower_bound, higher_bound):
+    optimization_models = ['SLSQP', 'Monte Carlo', 'COBYLA']
+    strategies = ['Minimum Variance', 'Omega Ratio', 'Semivariance', 'Roy Safety First Ratio',
+                  'Sortino Ratio', 'Fama French', 'CVaR', 'HRP', 'Sharpe Ratio',
+                  'Black Litterman', 'Total Return']
+    tickers_list = [ticker.strip() for ticker in tickers.split(',')]
+    results = {}
+
+    for strategy in strategies:
+        results[strategy] = pd.DataFrame(columns=['Modelo', 'Pesos Óptimos', 'Rendimiento Estimado', 'Volatilidad Estimada'])
+        for model in optimization_models:
+            qaa_instance = QAA(tickers=tickers_list, start_date=start_date, end_date=end_date, rf=rf, lower_bound=lower_bound, higher_bound=higher_bound)
+            qaa_instance.set_optimization_strategy(strategy)
+            qaa_instance.set_optimization_model(model)
+            qaa_instance.optimize()
+            if qaa_instance.optimal_weights is not None:
+                formatted_weights = [f"{weight * 100:.2f}%" for weight in qaa_instance.optimal_weights]
+                portfolio_return, portfolio_volatility = calculate_portfolio_metrics(qaa_instance)
+                results[strategy] = results[strategy].append({'Modelo': model, 'Pesos Óptimos': ", ".join(formatted_weights), 'Rendimiento Estimado': f"{portfolio_return:.2f}%", 'Volatilidad Estimada': f"{portfolio_volatility:.2f}%"}, ignore_index=True)
+            else:
+                results[strategy] = results[strategy].append({'Modelo': model, 'Pesos Óptimos': "No convergió o no implementado", 'Rendimiento Estimado': "N/A", 'Volatilidad Estimada': "N/A"}, ignore_index=True)
+
+    display_results(results)
+
+def calculate_portfolio_metrics(qaa_instance):
+    # Asigna valores de rendimiento y volatilidad a partir de la instancia QAA, asegúrate de que estos métodos estén implementados
+    portfolio_return = qaa_instance.calculate_portfolio_return()  # Debe ser implementado en QAA
+    portfolio_volatility = qaa_instance.calculate_portfolio_volatility()  # Debe ser implementado en QAA
+    return portfolio_return * 100, portfolio_volatility * 100
+
+def display_results(results):
+    for strategy, data in results.items():
+        st.subheader(f"{strategy}")
+        st.table(data) 
+
